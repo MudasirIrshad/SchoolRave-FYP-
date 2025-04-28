0f4a91c42b87dbee1b6e472341a9ccb5beaf4fb2
MudasirIrshad <mudasirirshad47@gmail.com>
2025-04-28 21:48:22 +0500

    school branch creation

---
+++ fyp/app/dashboard/school/[schoolId]/schoolHome/_components/(component)/(school_branch_components)/school_branch_list.tsx
@@ -0,0 +1,40 @@
+import { School, SchoolBranch as SchoolBranchType } from "@/generated/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { PencilIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface SchoolBranchProps {
+  initialData: SchoolBranchType[];
+  schoolId: string;
+}
+
+async function SchoolBranch({ initialData, schoolId }: SchoolBranchProps) {
+  const router = useRouter();
+  return (
+    <div className="max-h-44 overflow-y-auto space-y-3 p-2 bg-gray-50 rounded-lg">
+      {initialData.map((branch) => (
+        <div
+          key={branch.id}
+          className="flex items-center justify-between p-3 bg-white shadow-sm rounded-md hover:shadow-md transition"
+        >
+          <div className="text-sm font-semibold text-gray-800 truncate max-w-[75%]">
+            {branch.name || "Unnamed Branch"}
+          </div>
+          <button className="text-gray-500 hover:text-gray-700 transition">
+            <PencilIcon
+              onClick={() => {
+                router.push(
+                  `/dashboard/school/${schoolId}/schoolHome/schoolBranch/${branch.id}`
+                );
+              }}
+              className="w-4 h-4"
+            />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SchoolBranch;
---
+++ fyp/app/dashboard/school/[schoolId]/schoolHome/schoolBranch/[schoolBranchId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SchoolBranchPage() {
+  return (
+    <div>SchoolBranchPage</div>
+  )
+}
+
+export default SchoolBranchPage
