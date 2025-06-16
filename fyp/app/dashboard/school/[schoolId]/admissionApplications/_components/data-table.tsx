8437724bdec81dbd2201d5b89107697ad111eb7f
MudasirIrshad <mudasirirshad47@gmail.com>
2025-06-16 22:31:30 +0500

    verify payment

--- fyp/app/dashboard/school/[schoolId]/admissionApplications/_components/data-table.tsx
+++ fyp/app/dashboard/school/[schoolId]/admissionApplications/_components/data-table.tsx
@@ -55,7 +55,7 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4 justify-between">
         <Input
-          placeholder="Filter Courses..."
+          placeholder="Search Applications..."
           value={
             (table.getColumn("studentName")?.getFilterValue() as string) ?? ""
           }
